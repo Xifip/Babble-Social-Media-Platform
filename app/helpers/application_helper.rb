b67d812f6ee7122397d314ec3ba7ed9c20a5f990
Lloyd <lloydbme@lloyds-macbook.(none)>
2011-07-22 07:49:23 -0400

    Removed the default Rails index welcome page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  #function for returning a title
+  def title
+    base_title = "Babble"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
