b16929a3053c500f26799ed7197a20329d37a2eb
Lloyd <lloydbme@lloyds-macbook.(none)>
2011-08-02 21:07:41 -0400

    Finished adding User Signup page and tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,21 @@ class UsersController < ApplicationController
   end
   
   def new
+    @user = User.new
     @title = "Sign Up"
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    
+    if @user.save
+      flash[:success] = "welcome to babble !"
+      redirect_to @user
+    else
+      @title = "Sign Up"
+      render 'new'
+    end
+    
   end
 
 end
