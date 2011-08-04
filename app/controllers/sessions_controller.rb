53d88bbd4f77009279a1233bad8073c45588c7c3
Lloyd <lloydbme@lloyds-macbook.(none)>
2011-08-04 08:59:51 -0400

    Created sign in, sign out, profile link for when signed in, and session persistence via cookie

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+  
+  def new
+    @title = "Sign in"
+  end
+  
+  def create
+    
+    user = User.authenticate(params[:session][:email],params[:session][:password])
+    
+    if user.nil?
+      #render the sign in form with error message
+      flash.now[:error] = "Invalid email or password, please try again or create a new account"
+      @title = "Sign in"
+      render 'new'      
+    else
+      #sign the user in and render the user's show (profile) page    
+      sign_in user
+      redirect_to user
+    end
+    
+  end
+  
+  def destroy
+    sign_out
+    redirect_to root_path
+  end
+
+end
