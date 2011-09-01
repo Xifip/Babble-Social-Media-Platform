36ff6e8dad6117894d3a70519bd541cfe3f53271
Lloyd <lloydbme@lloyds-macbook.(none)>
2011-09-01 08:32:56 -0400

    Added integration tests for the followers and following pages

--- spec/requests/relationships_spec.rb
+++ spec/requests/relationships_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 describe "Relationships" do
   
   before(:each) do
-    user = Factory(:user)
+    @user1 = Factory(:user)
     @user2 = Factory(:user, :email => Factory.next(:email))
     visit signin_path
-    fill_in :email, :with => user.email
-    fill_in :password, :with => user.password
+    fill_in :email, :with => @user1.email
+    fill_in :password, :with => @user1.password
     click_button
   end
   
@@ -17,9 +17,23 @@ describe "Relationships" do
       
       it "user should follow user 2" do
         lambda do
-          visit user_path(@user2.id)
+          visit user_path(@user2)
           click_button
         end.should change(Relationship, :count).by(1)
+      end
+      
+      it "user's following page should have user2" do
+        visit user_path(@user2)
+        click_button
+        visit following_user_path(@user1)
+        response.should have_selector("li>div>strong", :content => @user2.name)          
+      end
+      
+      it "user2's follwers page should have user" do
+        visit user_path(@user2)
+        click_button
+        visit followers_user_path(@user2)
+        response.should have_selector("li>div>strong", :content => @user1.name) 
       end
       
     end    
@@ -29,7 +43,7 @@ describe "Relationships" do
   describe "unfollowing" do
     
     before(:each) do
-      visit user_path(@user2.id)
+      visit user_path(@user2)
       click_button
     end
     
@@ -37,10 +51,24 @@ describe "Relationships" do
       
       it "user should unfollow user 2" do
         lambda do
-          visit user_path(@user2.id)
+          visit user_path(@user2)
           click_button
         end.should change(Relationship, :count).by(-1)
       end   
+      
+      it "user's following page should not have user2" do
+        visit user_path(@user2)
+        click_button
+        visit following_user_path(@user1)
+        response.should_not have_selector("li>div>strong", :content => @user2.name)          
+      end
+      
+      it "user2's follwers page should not have user" do
+        visit user_path(@user2)
+        click_button
+        visit followers_user_path(@user2)
+        response.should_not have_selector("li>div>strong", :content => @user1.name) 
+      end
       
     end    
        
